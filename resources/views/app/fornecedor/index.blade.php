ec4f6e18b01941c86ed969a26c969704499b20d6
Arthur Albuquerque <arthurdev.me@gmail.com>
2023-05-19 00:56:15 -0300

    first commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,13 @@
+<h3>Fornecedor</h3>
+
+@php
+/*
+if(isset($variavel)) {} //retornar true se a variavel estiver definida
+*/
+@endphp
+
+Fornecedor: {{$fornecedores[0]['nome']}}
+<br>
+Status: {{$fornecedores[0]['status']}}
+<br>
+
