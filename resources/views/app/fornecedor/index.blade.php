4d52dc145fc3aa9eb730e53257598a6e5e22b7a4
Arthur Albuquerque <arthurdev.me@gmail.com>
2023-05-28 17:13:22 -0300

    Introdução do operador @for ao app

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -14,25 +14,15 @@ if(empty($variavel)) {} //retornar true se a variavel estiver vazia
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{$fornecedores[2]['nome']}}
-    <br>
-    Status: {{$fornecedores[2]['status']}}
-    <br>
-    CNPJ: {{$fornecedores[2]['CNPJ']}}
-    <br>
-    Telefone: {{$fornecedores[2]['ddd'] ?? '' }} {{$fornecedores[2]['telefone'] ?? '' }}
-    @switch($fornecedores[2]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('31')
-            Belo Horizonte - MG
-            @break
-        @case('83')
-            João Pessoa - PB
-            @break
-        @default
-            Estado não identificado
-    @endswitch
-
+      @for ($i = 0 ; isset($fornecedores[$i]) ; $i++)
+        Fornecedor: {{$fornecedores[$i]['nome']}}
+        <br>
+        Status: {{$fornecedores[$i]['status']}}
+        <br>
+        CNPJ: {{$fornecedores[$i]['CNPJ']}}
+        <br>
+        Telefone: {{$fornecedores[$i]['ddd'] ?? '' }} {{$fornecedores[$i]['telefone'] ?? '' }}
+        <hr>
+      @endfor
 @endisset
+   
