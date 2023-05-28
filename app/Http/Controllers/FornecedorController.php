6c8bd4a0f7581f78a86aed20685237c3c26b4790
Arthur Albuquerque <arthurdev.me@gmail.com>
2023-05-28 12:00:56 -0300

    introdução ao operador @empty

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,8 +9,15 @@ class FornecedorController extends Controller
   public function index()
   {
     $fornecedores = [
-      0 => ['nome' => 'Fornecedor1', 'status' => 'N'],
-
+      0 => [
+        'nome' => 'Fornecedor1',
+        'status' => 'N',
+        'CNPJ' => '00'
+      ],
+      1 => [
+        'nome' => 'Fornecedor 2',
+        'status' => 'S',
+      ]
     ];
 
     return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,12 +2,26 @@
 
 @php
 /*
-if(isset($variavel)) {} //retornar true se a variavel estiver definida
+if(empty($variavel)) {} //retornar true se a variavel estiver vazia
+- ''
+- 0
+- 0.0
+- null
+- false
+- array()
+- $var
 */
 @endphp
 
-Fornecedor: {{$fornecedores[0]['nome']}}
-<br>
-Status: {{$fornecedores[0]['status']}}
-<br>
-
+@isset($fornecedores)
+    Fornecedor: {{$fornecedores[0]['nome']}}
+    <br>
+    Status: {{$fornecedores[0]['status']}}
+    <br>
+    @isset($fornecedores[0]['CNPJ'])
+    CNPJ: {{$fornecedores[0]['CNPJ']}}
+        @empty($fornecedores[0]['CNPJ'])
+          - vazio
+        @endempty
+    @endisset
+@endisset
